ca9c2c8a13a3e5b9176d26a68dcc7b43d4c9a147
yuokada <callistoiv+git@gmail.com>
2018-08-10 22:03:23 +0900

    Remove header

--- src/main/java/io/github/yuokada/swagger/demoapi/impl/ApiResponseMessage.java
+++ src/main/java/io/github/yuokada/swagger/demoapi/impl/ApiResponseMessage.java
@@ -2,7 +2,6 @@ package io.github.yuokada.swagger.demoapi.impl;
 
 import javax.xml.bind.annotation.XmlTransient;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-06-29T20:45:33.761+09:00")
 
 @javax.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
--- src/main/java/io/github/yuokada/swagger/demoapi/impl/NotFoundException.java
+++ src/main/java/io/github/yuokada/swagger/demoapi/impl/NotFoundException.java
@@ -1,6 +1,5 @@
 package io.github.yuokada.swagger.demoapi.impl;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-06-29T20:45:33.761+09:00")
 
 public class NotFoundException extends ApiException {
     private int code;
